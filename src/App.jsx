2b47cc0b4581b61a05891fc3876581ae75613a25
prabhash1475 <prabhash.kumar96@gmail.com>
2025-06-04 15:12:56 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex items-center justify-center h-screen">CL_Frontend</h1>
+    </>
+  );
+}
+
+export default App;
