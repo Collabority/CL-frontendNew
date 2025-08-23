8c49ba89c9ef0bbd2aa8b49cdc390be9103df611
kratikesh18 <im.kartikesh18@gmail.com>
2025-08-24 01:48:37 +0530

    Integrated Reviews and services on portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Career from "./pages/Career";
 import Services from "./pages/Services";
