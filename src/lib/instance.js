8f9e61b7a5aee6efd2d8025a1908bfa89cd85d7c
yadnesh ranshevare <yranshevare2005@gmail.com>
2026-01-01 13:19:46 +0530

    add backend url

--- src/lib/instance.js
+++ src/lib/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://cl-adminpanelbackend.onrender.com",
   withCredentials: true,
 });
 
